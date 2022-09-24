b8f3b747a405f3023a629171cafe987075ec2085
Mathias Barbosa <76662048+mathiasbarbosa@users.noreply.github.com>
2022-09-24 17:22:39 -0300

    gif galeria, seccion nosotros, animaciones, ajustes mq

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ const generarInterfaz = (arr) => {
   let galery = document.querySelector("#galery")
   arr.map( el => {
     galery.innerHTML += `
-                        <div class="item-galery" style="background-image:url(${el.frame})";>
-                        <a class="item-description" href="${el.link}" target="_blank">
-                          <div>
-                            <p class="titulo">${el.titulo}</p>
-                            <p class="realizacion">${el.realizacion}</p>
-                            <p class="fecha">${el.anio}</p>
+                        <div class="item-galery"  data-aos="zoom-in-up"  data-aos-delay="250" style="background-image:url(${el.frame})"; > 
+                          <div class="item-container">
+                            <a class="item-description" href="${el.link}" target="_blank">
+                              <div>
+                                <p class="titulo">${el.titulo}</p>
+                                <p class="realizacion">${el.realizacion}</p>
+                                <p class="fecha">${el.anio}</p>
+                              </div>
+                            </a>
+                            <video src="./media/gif.mp4" class="item-gif"  autoplay loop muted></video>
                           </div>
-                        </a>
                       </div>
                       `
   })
 }
+
 
 fetch('./src/listaTrabajos.json')
   .then(response => response.json())
